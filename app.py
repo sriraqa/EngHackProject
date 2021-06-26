23a6b83e5d64bc1dad2fafde763c16f337de9b2f
sriraqa <qiaosarah8@gmail.com>
2021-06-26 18:20:03 -0400

    Fixed results display
    10 results display and they appear nicer.

--- app.py
+++ app.py
@@ -66,6 +66,20 @@ def get_recommendations():
 
     AUTH_URL = 'https://accounts.spotify.com/api/token'
 
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+
+    auth_response_data = auth_response.json()
+
+    access_token = auth_response_data['access_token']
+
+    headers = {
+    'Authorization': 'Bearer {token}'.format(token = access_token)
+    }
+
     BASE_URL = 'https://api.spotify.com/v1/'
 
     audio_req = requests.get(BASE_URL + 'audio-features/' + seed_tracks, headers = headers)
@@ -104,7 +118,7 @@ def get_recommendations():
 
 
     recommendation_endpoint = 'recommendations?' + 'seed_artists='+ seed_artists + '&seed_genres=' + seed_genres + '&seed_tracks=' + seed_tracks
-    added_features = '&danceability=' + str(r_danceability) + '&valence=' + str(r_valence) + '&instrumentalness=' +str(r_instrumentalness) + '&liveness=' + str(r_liveness) + '&speechiness=' + str(r_speechiness)
+    added_features = '&limit=10&danceability=' + str(r_danceability) + '&valence=' + str(r_valence) + '&instrumentalness=' +str(r_instrumentalness) + '&liveness=' + str(r_liveness) + '&speechiness=' + str(r_speechiness)
     
     recommendation_req = requests.get(BASE_URL + recommendation_endpoint + added_features, headers = headers)
     recommendation_json = json.loads(recommendation_req.text)
@@ -121,8 +135,9 @@ def get_recommendations():
         
     base = "https://open.spotify.com/embed/track/"
 
-    for i in range(20):
+    for i in range(10):
         info[i] = base + info[i][len("https://open.spotify.com/track/"):]
+
 
     return render_template('results.html', spotify_link=info)
 
